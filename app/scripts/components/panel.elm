969caa7632b6937d706961d6d0ca73f8cb8e3c52
Alessandro Delgado <adelgado1313@gmail.com>
2015-12-14 23:29:57 +0100

    Scaffold panel

--- app/scripts/components/panel.elm
+++ app/scripts/components/panel.elm
@@ -1,4 +1,10 @@
-import Html exposing (text)
+import Html                             exposing (..)
+import Html.Attributes                  exposing (..)
 
 main =
-  text "Hello, World!"
+    div [ class "panel" ]
+        [ div [ class "panel__title" ] [ text "This is a title" ]
+        , div [ class "panel__timestamp" ] [ ]
+        , img [ class "panel__image" ] [ ]
+        , p [ class "panel__description" ] [ ]
+        ]
