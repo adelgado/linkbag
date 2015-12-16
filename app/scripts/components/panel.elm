abf142810b168863eef7e146819de2b999ff1b09
Alessandro Delgado <adelgado1313@gmail.com>
2015-12-16 22:53:58 +0100

    Extract element values to variables

--- app/scripts/components/panel.elm
+++ app/scripts/components/panel.elm
@@ -2,9 +2,19 @@ import Html                             exposing (..)
 import Html.Attributes                  exposing (..)
 
 main =
-    div [ class "panel" ]
-        [ div [ class "panel__title" ] [ text "This is a title" ]
-        , div [ class "panel__timestamp" ] [ ]
-        , img [ class "panel__image" ] [ ]
-        , p [ class "panel__description" ] [ ]
-        ]
+    let
+        title = "This is a title"
+        imageUrl =
+            "http://www.joomlaworks.net/images/demos/galleries/abstract/7.jpg"
+        dateCreated = "1st of October of 1845"
+        description = "This was something that happened once because of bla and"
+    in
+        section
+            [ class "panel" ]
+            [ img [ class "panel__image", src imageUrl ] [ ]
+            , h2 [ class "panel__title" ] [ text title ]
+            , time [ class "panel__timestamp" ] [ text dateCreated ]
+            , p [ class "panel__description" ]
+                [ text description ]
+            ]
+
